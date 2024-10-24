648a8bbebd8714474713247f51cb55da8da23460
MultiMote <contact@mmote.ru>
2024-10-24 16:14:20 +0300

    Do not render IconPicker contents until it's open

--- src/lib/IconPicker.svelte
+++ src/lib/IconPicker.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+  import { onDestroy, onMount } from "svelte";
   import { tr } from "../utils/i18n";
   import { iconCodepoints, type MaterialIcon } from "../mdi_icons";
   import MdIcon from "./MdIcon.svelte";
@@ -8,13 +8,24 @@
 
   let iconNames: MaterialIcon[] = [];
   let search: string = "";
+  let dropdown: Element;
 
-  onMount(async () => {
-    iconNames = Object.keys(iconCodepoints) as MaterialIcon[];
+  const onShow = () => {
+    if (iconNames.length === 0) {
+      iconNames = Object.keys(iconCodepoints) as MaterialIcon[];
+    }
+  };
+
+  onMount(() => {
+    dropdown.addEventListener("show.bs.dropdown", onShow);
+  });
+
+  onDestroy(() => {
+    dropdown.removeEventListener("show.bs.dropdown", onShow);
   });
 </script>
 
-<div class="dropdown">
+<div class="dropdown" bind:this={dropdown}>
   <button class="btn btn-sm btn-secondary" data-bs-toggle="dropdown" data-bs-auto-close="outside">
     <MdIcon icon="emoji_emotions" />
     <MdIcon icon="add" />
@@ -23,11 +34,7 @@
   <div class="dropdown-menu">
     <h6 class="dropdown-header">{$tr("editor.iconpicker.title")}</h6>
     <div class="p-3">
-      <input
-        type="text"
-        class="form-control"
-        placeholder={$tr("editor.iconpicker.search")}
-        bind:value={search} />
+      <input type="text" class="form-control" placeholder={$tr("editor.iconpicker.search")} bind:value={search} />
       <div class="icons">
         {#each iconNames as name}
           {#if !search || name.includes(search.toLowerCase())}
