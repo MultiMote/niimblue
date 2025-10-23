e31d1823fd36475c1c97e51a289fa1d76f41e2fe
MultiMote <contact@mmote.ru>
2025-10-23 17:52:20 +0300

    Clear firmware data after flashing

--- src/lib/basic/FirmwareUpdater.svelte
+++ src/lib/basic/FirmwareUpdater.svelte
@@ -7,21 +7,21 @@
   let fwVersion: string = "";
   let fwVersionValid: boolean = false;
   let fwProgress: string = "";
-  let firmwareData: Uint8Array | undefined;
-  let firmwareName: string = "";
+  let fwData: Uint8Array | undefined;
+  let fwName: string = "";
 
   $: fwVersionValid = /^\d+\.\d+$/.test(fwVersion);
 
   const browseFw = async () => {
     const file = await FileUtils.pickAndReadBinaryFile("bin");
-    firmwareData = file.data;
-    firmwareName = file.name;
+    fwData = file.data;
+    fwName = file.name;
 
-    const match = firmwareName.match(/(\d+\.\d+)/);
+    const match = fwName.match(/(\d+\.\d+)/);
 
     // For modern firmware images version is stored in header
-    if (firmwareData[0] === 0x18) {
-      const verNumber = (firmwareData[0x15] << 8) + firmwareData[0x14];
+    if (fwData[0] === 0x18) {
+      const verNumber = (fwData[0x15] << 8) + fwData[0x14];
       fwVersion = (verNumber / 100).toFixed(2);
     } else if (match) {
       fwVersion = match[1];
@@ -31,7 +31,7 @@
   };
 
   const upgradeFw = async () => {
-    if (firmwareData === undefined) {
+    if (fwData === undefined) {
       return;
     }
 
@@ -48,11 +48,15 @@
     try {
       $printerClient.on("firmwareprogress", listener);
       fwProgress = "...";
-      await $printerClient.abstraction.firmwareUpgrade(firmwareData, fwVersion);
+      await $printerClient.abstraction.firmwareUpgrade(fwData, fwVersion);
       $printerClient.off("firmwareprogress", listener);
       await $printerClient.disconnect();
 
       Toasts.message("Flashing is finished, the printer will shut down now");
+      
+      fwData = undefined;
+      fwName = "";
+      fwVersion = "";
     } catch (e) {
       $printerClient.startHeartbeat();
       $printerClient.off("firmwareprogress", listener);
@@ -70,8 +74,8 @@
       <span class="input-group-text">Uploading {fwProgress}</span>
     {:else}
       <span class="input-group-text">To</span>
-      <button class="btn btn-sm btn-secondary" title={firmwareName} on:click={browseFw} disabled={!!fwProgress}>
-        {firmwareName.length > 0 ? firmwareName.slice(0, 8) + "..." : "Browse..."}
+      <button class="btn btn-sm btn-secondary" title={fwName} on:click={browseFw} disabled={!!fwProgress}>
+        {fwName.length > 0 ? fwName.slice(0, 8) + "..." : "Browse..."}
       </button>
       <span class="input-group-text">ver.</span>
       <input class="form-control" placeholder="x.x" type="text" size="6" bind:value={fwVersion} />
@@ -79,7 +83,7 @@
       <button
         class="btn btn-sm btn-danger"
         on:click={upgradeFw}
-        disabled={!!fwProgress || !fwVersionValid || firmwareData === undefined}>Burn</button>
+        disabled={!!fwProgress || !fwVersionValid || fwData === undefined}>Burn</button>
     {/if}
   </div>
 </div>
