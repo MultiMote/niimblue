29760e4cdd4ca91ad21d16efc2252bcfa3404409
MultiMote <contact@mmote.ru>
2025-11-24 17:47:29 +0300

    Enable runes explicitly

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,9 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
 const config = {
+  compilerOptions: {
+    runes: true
+  },
   preprocess: vitePreprocess(),
 };
 
