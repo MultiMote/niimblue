182e7e089fdaab868d82aba6ec451c203613f497
MultiMote <contact@mmote.ru>
2025-11-24 17:41:50 +0300

    Remove FabricConfig.disableStyleCopyPaste duplicate

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
   import MainPage from "$/components/MainPage.svelte";
-  import { config as FabricConfig } from "fabric";
-
-  FabricConfig.disableStyleCopyPaste = true;
 </script>
 
 <MainPage />
