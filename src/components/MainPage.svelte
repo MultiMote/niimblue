e679313b35af2151105e4fce26af3f3fcaa042f0
wallalex0 <alexander@jwallrodt.de>
2026-06-08 00:35:30 +0200

    prohibit content overlapping footer

--- src/components/MainPage.svelte
+++ src/components/MainPage.svelte
@@ -17,52 +17,54 @@
   let debugStuffShow = $state<boolean>(false);
 </script>
 
-<div class="container my-2">
-  <div class="row align-items-center mb-3">
-    <div class="col">
-      <h1 class="title">
-        <span class="niim">Niim</span><span class="blue">Blue{isStandalone ? "s" : ""}</span>
-      </h1>
+<div class="page-wrapper d-flex flex-column min-vh-100">
+  <div class="container my-2 flex-grow-1">
+    <div class="row align-items-center mb-3">
+      <div class="col">
+        <h1 class="title">
+          <span class="niim">Niim</span><span class="blue">Blue{isStandalone ? "s" : ""}</span>
+        </h1>
+      </div>
+      <div class="col-md-3">
+        <PrinterConnector />
+      </div>
     </div>
-    <div class="col-md-3">
-      <PrinterConnector />
+    <div class="row">
+      <div class="col">
+        <BrowserWarning />
+      </div>
     </div>
-  </div>
-  <div class="row">
-    <div class="col">
-      <BrowserWarning />
+
+    <div class="row">
+      <div class="col">
+        <LabelDesigner />
+      </div>
     </div>
   </div>
 
-  <div class="row">
-    <div class="col">
-      <LabelDesigner />
+  <div class="footer text-end text-secondary p-3">
+    <div>
+      <select class="form-select form-select-sm text-secondary d-inline-block w-auto" bind:value={$locale}>
+        {#each Object.entries(locales) as [key, name] (key)}
+          <option value={key}>{name}</option>
+        {/each}
+      </select>
     </div>
-  </div>
-</div>
-
-<div class="footer text-end text-secondary p-3">
-  <div>
-    <select class="form-select form-select-sm text-secondary d-inline-block w-auto" bind:value={$locale}>
-      {#each Object.entries(locales) as [key, name] (key)}
-        <option value={key}>{name}</option>
-      {/each}
-    </select>
-  </div>
-  <div>
-    {#if appCommit}
-      <a class="text-secondary" href="https://github.com/MultiMote/niimblue/commit/{appCommit}">
-        {appCommit.slice(0, 6)}
-      </a>
-    {/if}
-    {$tr("main.built")}
-    {buildDate}
-  </div>
-  <div>
-    <a class="text-secondary" href="https://github.com/MultiMote/niimblue">{$tr("main.code")}</a>
-    <button class="text-secondary btn btn-link p-0" onclick={() => debugStuffShow = true}>
-      <MdIcon icon="bug_report" />
-    </button>
+    <div>
+      {#if appCommit}
+        <a class="text-secondary" href="https://github.com/MultiMote/niimblue/commit/{appCommit}">
+          {appCommit.slice(0, 6)}
+        </a>
+      {/if}
+      {$tr("main.built")}
+      {buildDate}
+    </div>
+    <div>
+      <a class="text-secondary" href="https://github.com/MultiMote/niimblue">{$tr("main.code")}</a>
+      <button class="text-secondary btn btn-link p-0" onclick={() => debugStuffShow = true}>
+        <MdIcon icon="bug_report" />
+      </button>
+    </div>
   </div>
 </div>
 
@@ -80,16 +82,5 @@
   }
 
   .footer {
-    position: absolute;
-    bottom: 0;
-    right: 0;
-    z-index: -1;
-  }
-
-  @media only screen and (max-device-width: 540px) {
-    .footer {
-      position: relative !important;
-      z-index: 0 !important;
-    }
   }
 </style>
